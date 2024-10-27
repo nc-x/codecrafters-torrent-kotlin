c71ca3de5b0bfd12198084c1c11b213ac1e08b52
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-10-27 07:44:33 +0530

    add support for decoding bencode format

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,18 @@
 import com.google.gson.Gson;
+
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
 val gson = Gson()
 
 fun main(args: Array<String>) {
-    val command = args[0]
-    when (command) {
+    when (val command = args[0]) {
         "decode" -> {
-            val bencodedValue = args[1]
-            val decoded = decodeBencode(bencodedValue)
+            val bencoded = args[1]
+            val decoded = decode(bencoded)
             println(gson.toJson(decoded))
             return
         }
-        else -> println("Unknown command $command")
-    }
-}
 
-fun decodeBencode(bencodedString: String): String {
-    when {
-        Character.isDigit(bencodedString[0]) -> {
-            val firstColonIndex = bencodedString.indexOfFirst { it == ':' }
-            val length = Integer.parseInt(bencodedString.substring(0, firstColonIndex))
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
-        }
-        else -> TODO("Only strings are supported at the moment")
+        else -> println("Unknown command $command")
     }
 }
